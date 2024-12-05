99106500af89c15411d6ce1f85ae12b6796a16ef
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-12-05 11:28:47 +0530

    headers

--- frontend/src/headers/Leftside.jsx
+++ frontend/src/headers/Leftside.jsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button"
+import dayjs from "dayjs"
 import { Menu } from "lucide-react"
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
+
+
 
 function Leftside() {
+
+  // const handleTodayClick = () => {
+
+  // }
+
+
   return (
     <div className="flex gap-3 items-center">
-      <div className="hidden item-center lg:flex">
+      <div className="hidden item-center lg:flex gap-2">
         <Button variant="goast" className="rounded-full p-2">
            <Menu className="size-6"/>
         </Button>
@@ -14,7 +25,26 @@ function Leftside() {
           height={40}
           alt="icon"
         />
+        <h1 className="text-2xl">Calendar</h1>
       </div>
+
+        <Button variant="outline">
+          Today
+        </Button>
+
+      <div className="flex items-center gap-3">
+        <MdKeyboardArrowLeft
+           className="size-6 cursor-pointer font-bold"
+        />
+
+         <MdKeyboardArrowRight
+           className="size-6 cursor-pointer font-bold"
+        />
+      </div>
+
+      <h1 className="text-xl hidden sm:block">
+        December 5, 2024
+      </h1>
     </div>
   )
 }
--- frontend/src/headers/Rightside.jsx
+++ frontend/src/headers/Rightside.jsx
@@ -1,9 +1,29 @@
+import * as React from "react"
+ 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 const Rightside = () => {
   return (
-    <div>
-      <h1>Right button</h1>
-    </div>
+    <Select>
+    <SelectTrigger className="w-24 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring">
+      <SelectValue placeholder="Month" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectItem value="month">Month</SelectItem>
+        <SelectItem value="week">Week</SelectItem>
+        <SelectItem value="day">Day</SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
   )
 }
 
